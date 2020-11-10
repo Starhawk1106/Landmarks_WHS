09e637fd6d181aeafb71769bc5cbb37975eaf66f
Will Stanley <whs17a@acu.edu>
2020-11-09 23:51:27 -0600

    Initial Commit

---
+++ Landmarks_WHS/Landmarks_WHSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Landmarks_WHSApp.swift
+//  Landmarks_WHS
+//
+//  Created by William Stanley on 11/9/20.
+//
+
+import SwiftUI
+
+@main
+struct Landmarks_WHSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
