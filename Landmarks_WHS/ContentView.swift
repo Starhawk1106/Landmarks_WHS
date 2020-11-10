0cc6eb66451bb42f824b3e4b55e6f10334d091a0
Will Stanley <whs17a@acu.edu>
2020-11-09 23:53:46 -0600

    The beginning

--- Landmarks_WHS/ContentView.swift
+++ Landmarks_WHS/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("Hello, time to code everything!")
             .padding()
     }
 }
