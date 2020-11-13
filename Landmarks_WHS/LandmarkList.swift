7c313fc2b084ecaa899d86f8f2cecfeb9557c7f3
Will Stanley <whs17a@acu.edu>
2020-11-12 19:24:50 -0600

    Building Lists & Other stuff

---
+++ Landmarks_WHS/LandmarkList.swift
@@ -0,0 +1,28 @@
+//
+//  LandmarkList.swift
+//  Landmarks_WHS
+//
+//  Created by William Stanley on 11/12/20.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarkData) { landmark in
+                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationBarTitle(Text("Landmarks"))
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+            .previewDevice(PreviewDevice(rawValue: "iPhone SE (2nd generation" ))
+    }
+}
--- Landmarks_WHS/Landmarks_WHSApp.swift
+++ Landmarks_WHS/Landmarks_WHSApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Landmarks_WHSApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LandmarksList()
         }
     }
 }
